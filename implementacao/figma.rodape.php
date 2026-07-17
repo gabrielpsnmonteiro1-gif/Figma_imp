f102124660a3f2f0ce5c02654f78c37ad1664b27
Gmont_grippa <gabriel.psnmonteiro1@gmail.com>
2026-07-16 21:51:37 -0300

    Add files via upload

---
+++ implementacao/figma.rodape.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
+    <section class="bg-dark">
+    <div class="container-lg bg-dark text-white py-5">
+        <div class="row g-0 align-items-start text-white">
+            <div class="col-lg-3 border-end px-4">
+                <img class="img-fluid" src="c:\Users\Aluno\Downloads\ChatGPT Image 16 de jul. de 2026, 19_12_49.png" alt="...">
+               
+
+                <i class="bi bi-facebook" style="font-size: 2rem; color: rgb(255, 255, 255);"></i>
+                <i class="bi bi-instagram" style="font-size: 2rem; color: rgb(255, 255, 255);"></i>
+                <i class="bi bi-youtube" style="font-size: 2rem; color: rgb(255, 255, 255);"></i>
+                <i class="bi bi-anthropic" style="font-size: 2rem; color: rgb(255, 255, 255);"></i>
+
+            </div>
+
+            <div class="col-lg-3 border-end px-4">
+                <h3 class="mb-3">Navegação</h3>
+                <div class="">
+                    <a  class="text-decoration-none" href="">HOME</a> <br>
+                    <a  class="text-decoration-none" href="">NOSSA ACADEMIA</a> <br>
+                    <a  class="text-decoration-none" href="">NOSSOS NOSSOS SERVIÇOS</a> <br>
+                    <a  class="text-decoration-none" href="">NOSSOS PRODUTOS</a> <br>
+                    <a  class="text-decoration-none" href="">FALE CONOSCO</a> <br>
+                </div>   
+            </div>
+
+            <div class="col-lg-3 border-end px-4">
+                <h3 class="mb-3">Endereço</h3>
+                    <div class="d-flex align-items-start">
+                        <i class="bi bi-geo-alt-fill"></i>
+                        <p class="fs-6">Av. das Inovações, 1234Sala 501 - Bairro TechVilleSão Paulo - SP</p>
+                    </div>
+
+                    <div class="d-flex align-items-start">
+                        <i class="bi bi-telephone"></i>
+                        <p>(11) 4002-8922</p>
+                    </div>
+
+                    <div class="d-flex align-items-start">
+                        <i class="bi bi-envelope"></i>
+                        <p>contato@technova.com.br</p>
+                    </div>
+            </div>
+
+            <div class="col-lg-3 border-end px-4">
+                <h3 class="mb-3">Horário de Atendimento</h3>
+                <div class="d-flex align-items-start">
+                    <i class="bi bi-clock"></i>
+                    <p>Segunda á Sexta-Feria <br>
+                       08:00 ás 18:00 
+                    </p>
+
+                  
+                
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+ <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+</body>
+</html>
