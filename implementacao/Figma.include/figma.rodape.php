9e9defa4478ce7c0df0fd31735aa52cd2a23b550
Gmont_grippa <gabriel.psnmonteiro1@gmail.com>
2026-07-23 21:37:07 -0300

    Add files via upload

---
+++ implementacao/Figma.include/figma.rodape.php
@@ -0,0 +1,59 @@
+
+<section class="bg-dark">
+    <div class="container-lg bg-dark text-white py-5">
+        <div class="row g-0 align-items-start text-white">
+            <div class="col-lg-3 border-end px-4">
+                <img class="img-fluid" src="c:\Users\Aluno\Downloads\ChatGPT Image 16 de jul. de 2026, 19_12_49.png" alt="...">
+               
+
+                <i class="bi bi-facebook" style="font-size: 2rem; color: rgb(255, 255, 255);"></i>
+                <i class="bi bi-instagram" style="font-size: 2rem; color: rgb(255, 255, 255);"></i>
+                <i class="bi bi-youtube" style="font-size: 2rem; color: rgb(255, 255, 255);"></i>
+                <i class="bi bi-anthropic" style="font-size: 2rem; color: rgb(255, 255, 255);"></i>
+
+            </div>
+
+            <div class="col-lg-3 border-end px-4">
+                <h3 class="mb-3">Navegação</h3>
+                <div class="">
+                    <a  class="text-decoration-none" href="">HOME</a> <br>
+                    <a  class="text-decoration-none" href="">NOSSA ACADEMIA</a> <br>
+                    <a  class="text-decoration-none" href="">NOSSOS NOSSOS SERVIÇOS</a> <br>
+                    <a  class="text-decoration-none" href="">NOSSOS PRODUTOS</a> <br>
+                    <a  class="text-decoration-none" href="">FALE CONOSCO</a> <br>
+                </div>   
+            </div>
+
+            <div class="col-lg-3 border-end px-4">
+                <h3 class="mb-3">Endereço</h3>
+                    <div class="d-flex align-items-start">
+                        <i class="bi bi-geo-alt-fill"></i>
+                        <p class="fs-6">Av. das Inovações, 1234Sala 501 - Bairro TechVilleSão Paulo - SP</p>
+                    </div>
+
+                    <div class="d-flex align-items-start">
+                        <i class="bi bi-telephone"></i>
+                        <p>(11) 4002-8922</p>
+                    </div>
+
+                    <div class="d-flex align-items-start">
+                        <i class="bi bi-envelope"></i>
+                        <p>contato@altaforma.com.br</p>
+                    </div>
+            </div>
+
+            <div class="col-lg-3 border-end px-4">
+                <h3 class="mb-3">Horário de Atendimento</h3>
+                <div class="d-flex align-items-start">
+                    <i class="bi bi-clock"></i>
+                    <p>Segunda á Sexta-Feria <br>
+                       08:00 ás 22:00 
+                    </p>
+
+                  
+                
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
---
+++ implementacao/Figma.include/figma.topo.php
@@ -0,0 +1,90 @@
+
+     <header class="bg-black">
+        <div class="container">
+
+            <nav class="navbar navbar-expand-lg navbar-dark py-3">
+
+                
+                <a class="navbar-brand d-flex align-items-center"  href="#">
+                    <img src="c:\Users\Aluno\Downloads\ChatGPT Image 16 de jul. de 2026, 19_12_49.png" class=" img-fluid rounded-circle" style="max-width: 128px; max-height: 128px;">
+
+                    <div class="ms-2">
+                        <h4 class="mb-0 fw-bold"></h4>
+                        <small class="text-secondary"></small>
+                    </div>
+                </a>
+
+                
+                <button class="navbar-toggler"
+                        type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target="#menu">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                
+                <div class="collapse navbar-collapse" id="menu">
+
+                    <ul class="navbar-nav ms-auto align-items-lg-center">
+
+                        <li class="nav-item mx-lg-2">
+                            <a class="nav-link active text-primary" href="#">
+                                HOME
+                            </a>
+                        </li>
+
+                        <li class="nav-item mx-lg-2">
+                            <a class="nav-link" href="#">
+                                NOSSOS SERVIÇOS
+                            </a>
+                        </li>
+
+                        <li class="nav-item dropdown mx-lg-2">
+                            <a class="nav-link dropdown-toggle"
+                            href="#"
+                            data-bs-toggle="dropdown">
+                                NOSSOS PRODUTOS
+                            </a>
+
+                            <ul class="dropdown-menu">
+                                <li>
+                                    <a class="dropdown-item" href="#">
+                                        MUSCULAÇÃO
+                                    </a>
+                                </li>
+
+                                <li>
+                                    <a class="dropdown-item" href="#">
+                                        CROSSFIT
+                                    </a>
+                                </li>
+
+                                <li>
+                                    <a class="dropdown-item" href="#">
+                                        ZUMBA
+                                    </a>
+                                </li>
+
+                                <li>
+                                    <a class="dropdown-item" href="#">
+                                        PILATES
+                                    </a>
+                                </li>
+                            </ul>
+                        </li>
+
+                        <li class="nav-item mx-lg-2">
+                            <a class="nav-link" href="#">
+                                fALE CONOSCO
+                            </a>
+                        </li>
+
+                    </ul>
+
+                </div>
+
+            </nav>
+
+        </div>
+    </header>
+
